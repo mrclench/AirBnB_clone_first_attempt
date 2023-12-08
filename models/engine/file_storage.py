6bf1dd006146febd5e270ac2f229a427c4ad5461
mrclench <charlesnnnortey@gmail.com>
2023-12-08 01:44:40 +0000

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""File Storage"""
+
+import json
+import os
+import datetime
+
+
+class FileStorage:
+    """File storage class for serialization and deserialization of objects"""
+    __file_path = "data_storage.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary _objects"""
