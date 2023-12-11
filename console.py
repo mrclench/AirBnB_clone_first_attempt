776e57f876a459910d96ddf86f4091fa5e8b2774
mrclench <charlesnnnortey@gmail.com>
2023-12-11 01:29:41 +0000

    done

--- console.py
+++ console.py
@@ -11,6 +11,11 @@ class HBNBCommand(cmd.Cmd):
 	"""A cmd module for building custom shells that let a user work with a program interactively."""
 	prompt = '(hbnb) '
 
+	def __init__(self, storage=None):
+		super().__init__()
+		self.storage = storage
+		self.classes = ["BaseModel", "User", "Place", "Review", "State", "City", "Amenity"]
+
 	def do_EOF(self, line):
 		"Exit"
 		return True
@@ -75,6 +80,7 @@ class HBNBCommand(cmd.Cmd):
 			return
 
 		class_name = args[0]
+
 		if class_name not in storage.classes:
 			print("** class doesn't exist **")
 			return
@@ -85,11 +91,63 @@ class HBNBCommand(cmd.Cmd):
 
 		instance_id = args[1]
 		key = "{].{}".format(class_name, instance_id)
+
 		all_objects = storage.all()
 
 		if key not in all_objects:
 			print("** no instance found **")
 			return
+
+		del all_objects[key]
+		storage.save()
+
+	def do_all(self, line):
+		"""Prints 'all' string representation of 'all' instances based or not on the class name"""
+		args = line.split()
+		if not args or args[0] not in self.classes:
+			print("** class doesn't exist **")
+			return
+
+		class_name = args[0]
+		all_objects = storage.all()
+		instances = [str(obj) for obj in all_objects.values() if type(obj).__name__ == class_name]
+		print(instances)
+
+	def do_update(self, line):
+		"""Updates an instance of the class name and id by adding or updating attribute."""
+		args = line.split()
+		if not args or args[0] not in self.classes:
+			print("** class name missing **" if not args else "** class doesn't exist **")
+			return
+
+		if len(args) < 2:
+			print("** instance id missing **")
+			return
+
+		class_name = args[0]
+		instance_id = args[1]
+		all_objects = storage.all()
+
+		key = "{}.{}".format(class_name, instance_id)
+		if key not in all_objects:
+			print("** no instance found **")
+			return
+
+		if len(args) < 3:
+			print("** attribute name missing **")
+			return
+
+		attr_name = args[2]
+		if len(args) < 4:
+			print("** value missing **")
+			return
+
+		attr_value = args[3]
+		instance = all_objects[key]
+
+		instance.save()
+
+
 
 
 if __name__ == '__main__':
