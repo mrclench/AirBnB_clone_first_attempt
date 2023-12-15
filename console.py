697fe7b29176bd634f77981fe218ed24b0132696
mrclench <charlesnnnortey@gmail.com>
2023-12-15 17:28:37 -0600

    done

--- console.py
+++ console.py
@@ -49,113 +49,113 @@ class HBNBCommand(cmd.Cmd):
         "Exit"
         return True
 
-	def do_quit(self, line):
-		'"Quit command to exit the program"\n'
-		return True
+    def do_quit(self, line):
+        '"Quit command to exit the program"\n'
+        return True
 
-	def emptyline(self):
-		pass
+    def emptyline(self):
+        pass
 
-	def do_show(self, line):
-		"""Prints the string representation of an instance based on the class name and class id"""
-		args = line.split()
-		if not args or args[0] not in self.classes:
-			print("** class name missing **")
-			return
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the class name and class id"""
+        args = line.split()
+        if not args or args[0] not in self.classes:
+            print("** class name missing **")
+            return
 
-		class_name = args[0]
-		if len(args) < 2:
-			print("** instance id missing **")
-			return
+        class_name = args[0]
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
 
-		instance_id = args[1]
-		key = "{}.{}".format(class_name, instance_id)
-		all_objects = storage.all()
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        all_objects = storage.all()
 
-		if key not in all_objects:
-			print("** no instance found **")
-			return
+        if key not in all_objects:
+            print("** no instance found **")
+            return
 
-		instance = all_objects[key]
-		print(instance)
+        instance = all_objects[key]
+        print(instance)
 
-	def do_destroy(self, line):
-		"""Deletes an instance based on the class name and id (saves update to the JSON file)."""
-		args = line.split()
-		if not args:
-			print("** class name missing **")
-			return
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id (saves update to the JSON file)."""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
 
-		class_name = args[0]
+        class_name = args[0]
 
-		if class_name not in HBNBCommand.__classes_allowed:
-			print("** class doesn't exist **")
-			return
+        if class_name not in HBNBCommand.__classes_allowed:
+            print("** class doesn't exist **")
+            return
 
-		if len(args) < 2:
-			print("** instance id missing **")
-			return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
 
-		instance_id = args[1]
-		key = "{}.{}".format(class_name, instance_id)
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
 
-		all_objects = storage.all()
+        all_objects = storage.all()
 
-		if key not in all_objects:
-			print("** no instance found **")
-			return
+        if key not in all_objects:
+            print("** no instance found **")
+            return
 
-		del all_objects[key]
-		storage.save()
+        del all_objects[key]
+        storage.save()
 
-	def do_all(self, command_args):
-		"""Prints string representation of all instances in the console"""
-		class_name = command_args.split()[0] if command_args else None
-		stored_objects = storage.all()
+    def do_all(self, command_args):
+        """Prints string representation of all instances in the console"""
+        class_name = command_args.split()[0] if command_args else None
+        stored_objects = storage.all()
 
-		if class_name and class_name not in self.__classes_allowed:
-			print("** class doesn't exist **")
-			return
+        if class_name and class_name not in self.__classes_allowed:
+            print("** class doesn't exist **")
+            return
 
-		filtered_objects = [str(obj) for key, obj in stored_objects.items() if
-							not class_name or key.split('.')[0] == class_name]
-		print(filtered_objects)
+        filtered_objects = [str(obj) for key, obj in stored_objects.items() if
+                                not class_name or key.split('.')[0] == class_name]
+        print(filtered_objects)
 
-	def do_update(self, command_args):
-		"""Updates an instance of the class name and id by adding or updating attribute."""
-		args = command_args.split()
-		if not args or args[0] not in self.classes:
-			print("** class name missing **" if not args else "** class doesn't exist **")
-			return
+    def do_update(self, command_args):
+        """Updates an instance of the class name and id by adding or updating attribute."""
+        args = command_args.split()
+        if not args or args[0] not in self.classes:
+            print("** class name missing **" if not args else "** class doesn't exist **")
+            return
 
-		if len(args) < 2:
-			print("** instance id missing **")
-			return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
 
-		class_name = args[0]
-		instance_id = args[1]
-		all_objects = storage.all()
+        class_name = args[0]
+        instance_id = args[1]
+        all_objects = storage.all()
 
-		key = "{}.{}".format(class_name, instance_id)
-		if key not in all_objects:
-			print("** no instance found **")
-			return
+        key = "{}.{}".format(class_name, instance_id)
+        if key not in all_objects:
+            print("** no instance found **")
+            return
 
-		if len(args) < 3:
-			print("** attribute name missing **")
-			return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
 
-		attr_name = args[2]
-		if len(args) < 4:
-			print("** value missing **")
-			return
+        attr_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
 
-		attr_value = args[3].strip('"')
-		#instance = all_objects[key]
+        attr_value = args[3].strip('"')
+        #instance = all_objects[key]
 
 		# Update the attribute in the instance
 		setattr(instance, attr_name, attr_value)
-		instance.save()
+        instance.save()
 
 
 storage = FileStorage()
